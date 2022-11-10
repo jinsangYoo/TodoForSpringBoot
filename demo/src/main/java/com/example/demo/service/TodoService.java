b7eb6db50c715c4956eb252d39efcab7981746e4
jinsang <jinsang@nhndata.com>
2022-11-10 16:04:59 +0900

    나머지 CRUD 기능 구현

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,37 @@ public class TodoService {
     log.info("Entity Id: {} is saved.", entity.getId());
 
     return repository.findByUserId(entity.getUserId());
+  }
+  
+  public List<TodoEntity> retrieve(final String userId) {
+    return repository.findByUserId(userId);
+  }
+  
+  public List<TodoEntity> update(final TodoEntity entity) {
+    validate(entity);
+    
+    final Optional<TodoEntity> original = repository.findById(entity.getId());
+    original.ifPresent(todo -> {
+      todo.setTitle(entity.getTitle());
+      todo.setDone(entity.isDone());
+      repository.save(todo);
+    });
+    
+    return retrieve(entity.getUserId());
+  }
+  
+  public List<TodoEntity> delete(final TodoEntity entity) {
+    validate(entity);
+    
+    try {
+      repository.delete(entity);
+    } catch (Exception e) {
+      // TODO: handle exception
+      log.error("error deleting entity ", entity.getId(), e);
+      throw new RuntimeException("error deleting entity " + entity.getId());
+    }
+    
+    return retrieve(entity.getUserId());
   }
 
   private void validate(final TodoEntity entity) {
